af5e7dbbefb5f640bdae8a0899b0193871722ffd
gbiz0 <jardimbgustavo@gmail.com>
2023-07-18 22:06:06 -0300

    feat: change api service

--- src/app/services/times.service.ts
+++ src/app/services/times.service.ts
@@ -12,12 +12,6 @@ export class TimesService {
 
   getTimes(): Observable<any> {
 
-    const headers = new HttpHeaders({
-      'Authorization': 'Bearer test_fd685222e0ba3c43d446666fe38975',
-    });
-
-    const requestOptions = { headers: headers };
-
-    return this.httpClient.get<any>('https://api.api-futebol.com.br/v1/campeonatos/10/tabela', requestOptions);
+    return this.httpClient.get<any>('https://apiv3.apifootball.com/?action=get_standings&league_id=99&APIkey=8188079a72fd96a274555e4e1b52c86c5f54ef4a72ad6a66c6c547562595edfb');
   }
 }
