d9cb80c53f368457076e47e704cbfc607a060f7d
gbiz0 <jardimbgustavo@gmail.com>
2023-07-18 21:26:18 -0300

    feat: consuming api

--- src/app/services/times.service.ts
+++ src/app/services/times.service.ts
@@ -13,11 +13,11 @@ export class TimesService {
   getTimes(): Observable<any> {
 
     const headers = new HttpHeaders({
-      'Authorization': 'Bearer [test_fd685222e0ba3c43d446666fe38975]',
+      'Authorization': 'Bearer test_fd685222e0ba3c43d446666fe38975',
     });
 
     const requestOptions = { headers: headers };
 
-    return this.httpClient.get<any>('https://api.api-futebol.com.br/v1/times/', requestOptions);
+    return this.httpClient.get<any>('https://api.api-futebol.com.br/v1/campeonatos/10/tabela', requestOptions);
   }
 }
